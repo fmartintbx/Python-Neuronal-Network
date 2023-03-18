418e3371ecb4292b989cf33152567b0bd25f0964
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2023-03-18 03:18:52 -0300

    neuronal network

---
+++ neuronalnetwork.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Importando Datos
+temperature_df = pd.read_csv('temperaturas.csv')
+
+#Visualizando Datos
+sns.scatterplot(temperature_df['Celsius'], temperature_df['Fahrenheit'])
+
+#Cargando Set de Datos
+X_train = temperature_df['Celsius']
+y_train = temperature_df['Fahrenheit']
+#Creando el Modelo
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=1, input_shape=[1]))
+
+#model.summary()
+
+#Compilado
+model.compile(optimizer=tf.keras.optimizers.Adam(0.5), loss='mean_squared_error')
+
+#Entrenando el modelo
+epochs_hist = model.fit(X_train, y_train, epochs = 400)
+
+#Evaluando el modelo
+epochs_hist.history.keys()
+
+#Grafico
+plt.plot(epochs_hist.history['loss'])
+plt.title("Progreso de perdida durante el entrenamiento del modelo")
+plt.xlabel("Epoch")
+plt.ylabel("Training Loss")
+plt.legend("Training Loss")
+
+model.get_weights()
+#Predicciones
+Temp_C = 0
+Temp_F = model.predict([Temp_C])
+print(Temp_F)
+
+Temp_F = 9/5 * Temp_C + 32
+print(Temp_F)
+
